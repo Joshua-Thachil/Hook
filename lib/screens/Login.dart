5efaa41b04a6df31013ff9d744b440459c2ba234
sugarplummfairy <120628972+cherrykiwimango@users.noreply.github.com>
2024-08-10 22:38:17 +0530

    login and signUp screens

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:musicapp/auth/auth_service.dart';
 import 'package:musicapp/screens/HomePage.dart';
@@ -20,7 +21,7 @@ class _LoginState extends State<Login> {
     final user = await _auth.signWithEmailAndPassword(loginEmailController.text, loginPasswordController.text);
 
     if(user != null) {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+      Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
       dispose();
       print("User Logged in Successfully");
     }
@@ -41,65 +42,200 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff101010),
+      appBar: AppBar(
+        toolbarHeight: 120.0,
+        backgroundColor: Colors.transparent,
+        leading: const Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+          size: 50,
+        ),
+      ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(25.0),
+          padding: const EdgeInsets.only(left: 32, right: 32),
           child: ListView(
             children: [
-              SizedBox(height: 150,),
+              // SizedBox(height: 157,),
 
-              Text(
-                "Login",
-                style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold
+              const Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Welcome\n",
+                      style: TextStyle(
+                        fontSize: 57,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        height: 1.25,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "Back,\n",
+                      style: TextStyle(
+                        fontSize: 57,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff80AC97),
+                        height: 1.25,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "Log In.",
+                      style: TextStyle(
+                        fontSize: 57,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        height: 1.25,
+                      ),
+                    ),
+                  ]
+                )
+              ),
+
+              const SizedBox(height: 90,),
+
+              ElevatedButton(
+                onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  fixedSize: const Size(366, 70),
+                  backgroundColor: const Color(0xff1E1E1E),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.cached_sharp),
+                    Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Continue With ",
+                            style: TextStyle(
+                              fontSize: 18.3,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Google",
+                            style: TextStyle(
+                              fontSize: 18.3,
+                              color: Color(0xff80AC97),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ]
+                      )
+                    ),
+                  ],
+                )
+              ),
+
+              const SizedBox(height: 20,),
+
+              const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "or",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                  ),
                 ),
               ),
 
-              SizedBox(height: 50,),
+              const SizedBox(height: 20,),
 
               TextField(
                 controller: loginEmailController,
                 decoration: const InputDecoration(
-                  hintText: "Email",
-                  label: Icon(Icons.email_outlined),
+                  hintText: "email",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.3,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xff1E1E1E),
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(25))
-                  )
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 18.0),
                 ),
               ),
 
-              SizedBox(height: 25,),
+              const SizedBox(height: 25,),
 
               TextField(
                 controller: loginPasswordController,
                 decoration: const InputDecoration(
-                    hintText: "Password",
-                    label: Icon(Icons.password_outlined),
+                    hintText: "password",
+                    hintStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.3,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    filled: true,
+                    fillColor: Color(0xff1E1E1E),
                     border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(25))
-                    )
+                      borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                    ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 18.0),
                 ),
               ),
 
-              SizedBox(height: 25,),
+              const SizedBox(height: 15,),
+
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: (){},
+                  child: const Text(
+                    "forgot password?",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                    )
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 15,),
 
               ElevatedButton(
                   onPressed: () async{
                     await login();
                   },
-                  child: Text(
-                      "Login",
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: const Color(0xff90DAB9),
+                  fixedSize: const Size(366, 61.6),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                ),
+                  child: const Text(
+                      "Log In",
+                    style: TextStyle(
+                      fontSize: 18.3,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                   ),
               ),
 
-              SizedBox(height: 15,),
+              const SizedBox(height: 15,),
 
               ElevatedButton(
                 onPressed: () {
                   Navigator.push(context, MaterialPageRoute(builder: (context) => SignUp()));
                 },
-                child: Text(
+                child: const Text(
                   "Signup",
                 ),
               ),
--- lib/screens/SignUp.dart
+++ lib/screens/SignUp.dart
@@ -40,58 +40,192 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: ListView(
-          children: [
-            SizedBox(height: 150,),
-
-            Text(
-              "Sign Up",
-              style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-
-            SizedBox(height: 50,),
-
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                  hintText: "Email",
-                  label: Icon(Icons.email_outlined),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25))
+      backgroundColor: const Color(0xff101010),
+      appBar: AppBar(
+        toolbarHeight: 120.0,
+        backgroundColor: Colors.transparent,
+        leading: const Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+          size: 50,
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 32, right: 32),
+          child: ListView(
+            children: [
+              const Text.rich(
+                  TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "Join the\n",
+                          style: TextStyle(
+                            fontSize: 57,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            height: 1.25,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "Jam,\n",
+                          style: TextStyle(
+                            fontSize: 57,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff80AC97),
+                            height: 1.25,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "Sign Up.",
+                          style: TextStyle(
+                            fontSize: 57,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            height: 1.25,
+                          ),
+                        ),
+                      ]
                   )
               ),
-            ),
 
-            SizedBox(height: 25,),
+              const SizedBox(height: 90,),
 
-            TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                  hintText: "Password",
-                  label: Icon(Icons.password_outlined),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25))
+              ElevatedButton(
+                  onPressed: (){},
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    fixedSize: const Size(366, 70),
+                    backgroundColor: const Color(0xff1E1E1E),
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(Icons.cached_sharp),
+                      Text.rich(
+                          TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: "Continue With ",
+                                  style: TextStyle(
+                                    fontSize: 18.3,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                TextSpan(
+                                  text: "Google",
+                                  style: TextStyle(
+                                    fontSize: 18.3,
+                                    color: Color(0xff80AC97),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ]
+                          )
+                      ),
+                    ],
                   )
               ),
-            ),
 
-            SizedBox(height: 25,),
+              const SizedBox(height: 20,),
 
-            ElevatedButton(
-              onPressed: () async {
-                await signUp();
-            },
-              child: Text(
-                "Signup",
+              const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "or",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                  ),
+                ),
               ),
-            ),
 
-          ],
+              const SizedBox(height: 20,),
+
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  hintText: "email",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.3,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xff1E1E1E),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 18.0),
+                ),
+              ),
+
+              const SizedBox(height: 25,),
+
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  hintText: "password",
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.3,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xff1E1E1E),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 18.0),
+                ),
+              ),
+              const SizedBox(height: 15,),
+
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: (){},
+                  child: const Text(
+                      "forgot password?",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                      )
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 15,),
+
+              ElevatedButton(
+                onPressed: () async{
+                  await signUp();
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  backgroundColor: const Color(0xff90DAB9),
+                  fixedSize: const Size(366, 61.6),
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                ),
+                child: const Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    fontSize: 18.3,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+
+            ],
+          ),
         ),
       ),
     );
