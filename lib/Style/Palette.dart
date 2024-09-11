921f73e58a94b303c22ecc782a8afabcb0913f6d
aadzy <113158443+aadzy@users.noreply.github.com>
2024-09-11 12:21:53 +0530

    Create Palette.dart

---
+++ lib/Style/Palette.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+class Palette{
+
+var primary = Color(0xffA7F2D0);
+var secondary = Color(0xffCFE9DA);
+var bg = Color(0xff101010);
+var secondary_bg = Color(0xff1E1E1E);
+var accent = Color(0xff1D6B50);
+var primary_text = Color(0xffFFFFFF);
+
+
+
+}
